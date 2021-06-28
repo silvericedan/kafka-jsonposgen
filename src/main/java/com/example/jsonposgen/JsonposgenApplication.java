768be06ad7889a48284b8ac9d95d777f9406ae3e
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-06-28 11:54:06 -0300

    created data model

---
+++ src/main/java/com/example/jsonposgen/JsonposgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonposgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonposgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonposgenApplication.class, args);
+	}
+
+}
