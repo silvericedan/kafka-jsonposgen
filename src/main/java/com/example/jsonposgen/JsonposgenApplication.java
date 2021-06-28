73d8a655fb4564153e050d35b75ab5fb80f93adb
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-06-28 16:34:08 -0300

    added KafkaProducerService,application.yaml config and overrided main method

--- src/main/java/com/example/jsonposgen/JsonposgenApplication.java
+++ src/main/java/com/example/jsonposgen/JsonposgenApplication.java
@@ -1,13 +1,35 @@
 package com.example.jsonposgen;
 
+import com.example.jsonposgen.services.KafkaProducerService;
+import com.example.jsonposgen.services.datagenerator.InvoiceGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JsonposgenApplication {
+public class JsonposgenApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonposgenApplication.class, args);
 	}
 
+	@Autowired
+	private KafkaProducerService producerService;
+
+	@Autowired
+	private InvoiceGenerator invoiceGenerator;
+
+	@Value("${application.configs.invoice.count}")
+	private int INVOICE_COUNT;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		for (int i = 0; i<INVOICE_COUNT;i++){
+			producerService.sendMessage(invoiceGenerator.getNextInvoice());
+			Thread.sleep(1000);
+		}
+	}
 }
